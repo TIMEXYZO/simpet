abf94fe8b7eb033a3eaafb078ae1346904d073d6
TIMEXYZO <168674741+TIMEXYZO@users.noreply.github.com>
2025-03-25 03:29:02 +0530

    Create chapters.js

---
+++ chapters.js
@@ -0,0 +1,20 @@
+module.exports = {
+  Maths: [
+    "Sets", "Relations and Functions", "Trigonometric Functions", "Principle of Mathematical Induction",
+    "Complex Numbers and Quadratic Equations", "Linear Inequalities", "Permutations and Combinations",
+    "Binomial Theorem", "Sequences and Series", "Straight Lines", "Conic Sections",
+    "Introduction to Three Dimensional Geometry", "Limits and Derivatives", "Mathematical Reasoning",
+    "Statistics", "Probability"
+  ],
+  Chemistry: [
+    "Some Basic Concepts of Chemistry", "Structure of Atom", "Classification of Elements and Periodicity in Properties",
+    "Chemical Bonding and Molecular Structure", "Thermodynamics", "Equilibrium", "Redox Reactions",
+    "Organic Chemistry - Some Basic Principles and Techniques", "Hydrocarbons"
+  ],
+  Physics: [
+    "Units and Measurements", "Motion in a Straight Line", "Motion in a Plane", "Laws of Motion",
+    "Work, Energy, and Power", "System of Particles and Rotational Motion", "Gravitation",
+    "Mechanical Properties of Solids", "Mechanical Properties of Fluids", "Thermal Properties of Matter",
+    "Thermodynamics", "Kinetic Theory", "Oscillations", "Waves"
+  ]
+};
